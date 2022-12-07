a6e5c29de5329819138cb415f7b922568702ef2e
Ruslan <ruslan56781@mail.ru>
2022-12-07 15:11:16 +0300

    new table and little replace fragments code

--- Login/login.h
+++ Login/login.h
@@ -1,44 +1,48 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
-
 #include "airlinedb.h"
-#include "storage.h"
+#include "flights.h"
 #include "passengers.h"
+#include "storage.h"
+#include "tickets.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Login; }
+namespace Ui {
+class Login;
+}
 QT_END_NAMESPACE
 
-class Login : public QMainWindow
-{
-	Q_OBJECT
+class Login : public QMainWindow {
+  Q_OBJECT
 
 public:
-	explicit Login(QWidget* parent = nullptr);
-	~Login();
+  explicit Login(QWidget *parent = nullptr);
+  ~Login();
 
 public slots:
-	void DatabaseEntry() noexcept;
+  void DatabaseEntry() noexcept;
 
-	void ChoiceViewPassword() noexcept;
-
-public:
-	auto CheckCorrectInput() noexcept -> bool;
+  void ChoiceViewPassword() noexcept;
 
 public:
-	static Login* getInstance()noexcept;
+  auto CheckCorrectInput() noexcept -> bool;
+
+public:
+  static Login *getInstance() noexcept;
 
 private:
-	QScopedPointer<Storage> t_st;
-	Storage* m_st;
+  QScopedPointer<Storage> t_st;
+  Storage *m_st;
 
 private:
-	AirlineDB t_db;
-	Passengers p_db;
+  AirlineDB t_db;
+  Passengers p_db;
+  Flights f_db;
+  Tickets ts_db;
 
 private:
-	Ui::Login* ui;
+  Ui::Login *ui;
 };
 
 #endif // LOGIN_H
--- Login/src/login.cpp
+++ Login/src/login.cpp
@@ -4,92 +4,94 @@
 #include <QMainWindow>
 #include <QScopedPointer>
 
-Login::Login(QWidget* parent) :
-	QMainWindow(parent),
-	ui(new Ui::Login)
-{
-	ui->setupUi(this);
+Login::Login(QWidget *parent) : QMainWindow(parent), ui(new Ui::Login) {
+  ui->setupUi(this);
 
-	QObject::connect(ui->btnLogin, SIGNAL(clicked(bool)), this, SLOT(DatabaseEntry()));
-	QObject::connect(ui->btnPassword, SIGNAL(clicked(bool)), this,
-		SLOT(ChoiceViewPassword()));
+  QObject::connect(ui->btnLogin, SIGNAL(clicked(bool)), this,
+                   SLOT(DatabaseEntry()));
+  QObject::connect(ui->btnPassword, SIGNAL(clicked(bool)), this,
+                   SLOT(ChoiceViewPassword()));
 
-	m_st = new Storage;
+  m_st = new Storage;
 
-	QObject::connect(ui->ActChangePassword, &QAction::triggered, m_st, &Storage::ChangePassword);
-	QObject::connect(ui->actionClose, SIGNAL(triggered(bool)), qApp, SLOT(quit()));
+  QObject::connect(ui->ActChangePassword, &QAction::triggered, m_st,
+                   &Storage::ChangePassword);
+  QObject::connect(ui->actionClose, SIGNAL(triggered(bool)), qApp,
+                   SLOT(quit()));
 
-	delete m_st;
+  delete m_st;
 }
 
-Login::~Login()
-{
-	delete ui;
-}
+Login::~Login() { delete ui; }
 
 void Login::DatabaseEntry() noexcept {
-	QString password = ui->LinePassword->text();
-	QString login = ui->LineLogin->text();
+  QString password = ui->LinePassword->text();
+  QString login = ui->LineLogin->text();
 
-	if (CheckCorrectInput()) {
-		if (t_st->CorrectlyLogin(password, login)) {
-			int result = QInputDialog::getInt(this, tr("Choice one of need DataBase"),
-				tr("Enter: "));
+  if (CheckCorrectInput()) {
+    if (t_st->CorrectlyLogin(password, login)) {
+      int result = QInputDialog::getInt(this, tr("Choice one of need Table"),
+                                        tr("Enter: "));
 
-			switch (result) {
-			case 1:
-				t_db.ConnectAirline();
-				t_db.show();
-				break;
+      switch (result) {
+      case 1:
+        t_db.ConnectAirline();
+        t_db.show();
+        break;
 
-			case 2:
-				p_db.ConnectPassengers();
-				p_db.show();
-				break;
+      case 2:
+        p_db.ConnectPassengers();
+        p_db.show();
+        break;
 
-			default:
-				break;
-			}
+      case 3:
+        f_db.ConnectFlights();
+        f_db.show();
+        break;
 
+      case 4:
+        ts_db.ConnectTickets();
+        ts_db.show();
+        break;
 
-		}
-		else {
-			QMessageBox::warning(this, "The Error", "Authorisation Error");
-		}
-	}
-	else {
-		QMessageBox::warning(this, "The Error", "Login or password entered incorrectly");
-	}
+      default:
+        break;
+      }
+
+    } else {
+      QMessageBox::warning(this, "The Error", "Authorisation Error");
+    }
+  } else {
+    QMessageBox::warning(this, "The Error",
+                         "Login or password entered incorrectly");
+  }
 }
 
-
-Login* Login::getInstance() noexcept
-{
-	static Login instance;
-	return &instance;
+Login *Login::getInstance() noexcept {
+  static Login instance;
+  return &instance;
 }
 
 void Login::ChoiceViewPassword() noexcept {
-	if (ui->btnPassword->isChecked()) {
-		ui->LinePassword->setEchoMode(QLineEdit::Normal);
-	}
-	else {
-		ui->LinePassword->setEchoMode(QLineEdit::Password);
-	}
+  if (ui->btnPassword->isChecked()) {
+    ui->LinePassword->setEchoMode(QLineEdit::Normal);
+  } else {
+    ui->LinePassword->setEchoMode(QLineEdit::Password);
+  }
 }
 
 auto Login::CheckCorrectInput() noexcept -> bool {
-	if (ui->LinePassword->text().isEmpty()) {
-		QMessageBox::critical(this, "Error Authorisation",
-			"User doesn't password enter,input is ::empty()");
-		return false;
-	}
+  if (ui->LinePassword->text().isEmpty()) {
+    QMessageBox::critical(this, "Error Authorisation",
+                          "User doesn't password enter,input is ::empty()");
+    return false;
+  }
 
-	if (ui->LineLogin->text().isEmpty()) {
-		QMessageBox::critical(this, "Error Authorisation",
-			"User doesn't login enter,input is ::empty()");
-		return false;
-	}
+  if (ui->LineLogin->text().isEmpty()) {
+    QMessageBox::critical(this, "Error Authorisation",
+                          "User doesn't login enter,input is ::empty()");
+    return false;
+  }
 
-	return true;
+  return true;
 }
